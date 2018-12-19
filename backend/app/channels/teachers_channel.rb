ada2eac6ddc44fd4ecd99cdb17b67ee316d75b85
Keith McSpurren <info@track2development.com>
2018-12-19 18:20:51 -0500

    channels created

---
+++ backend/app/channels/teachers_channel.rb
@@ -0,0 +1,10 @@
+class TeachersChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+    stream_from "teachers_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+end
