e10fb884a79d2a4952010f923ff68ce678856d0b
Keith McSpurren <info@track2development.com>
2018-12-19 20:50:08 -0500

    channel modifications

--- backend/app/channels/students_channel.rb
+++ backend/app/channels/students_channel.rb
@@ -1,8 +1,15 @@
 class StudentsChannel < ApplicationCable::Channel
   def subscribed
     # stream_from "some_channel"
-    stream_from "student_channel"
+    @student = Student.find_by(id: params[:name])
+    stream_for @student
   end
+
+##
+#  def received(data)
+#    @teacher = how to we set this to go to the Teacher channel
+#    StudentsChannel.broadcast_to ( @teacher, #{and then what data goes here} )
+#   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
