476686b088ec68348bc9dcdaffb281feb65a7476
Keith McSpurren <info@track2development.com>
2018-12-20 14:36:58 -0500

    final catchups

--- backend/app/channels/teachers_channel.rb
+++ backend/app/channels/teachers_channel.rb
@@ -4,11 +4,17 @@ class TeachersChannel < ApplicationCable::Channel
     stream_from "teachers_channel"
   end
 
+  ## Outbound messages to teachers..
+
+  # change the state of the student object for the teacher as new students come online
   def who_is_online
   end
 
+  # broadcast the update to the individual student scoreboard (right, wrong, category and level)
   def student_answer
   end
+
+  # INBound messages
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
--- backend/app/controllers/teachers_controller.rb
+++ backend/app/controllers/teachers_controller.rb
@@ -1,11 +1,11 @@
 class TeachersController < ApplicationController
 
     def create
-    teacher = Teacher.create(user_params)
+      teacher = Teacher.create(user_params)
     end
 
     def show
-      @teacher = Teacher.find(params[:id])
+      @teacher = Teacher.find_by(name: params[:id])
       render json: @teacher
 #    @products = @category.products.order(created_at: :desc)
     end
