0031a1d1dca37b15f5c66ba23c83bb0399b33ca9
Aaron Lai <aaron_lai@hotmail.com>
2018-12-21 18:22:29 -0500

    modified buttons to be radial buttons, started logic for questions

--- frontend/src/student/questionHandler.jsx
+++ frontend/src/student/questionHandler.jsx
@@ -15,9 +15,9 @@ class QuestionHandler extends Component {
       a4: null,
       correct_answer: null,
       selected_answer: null,
-      level: 3,
+      level: 2,
       category_id: 1,
-      round: 0
+      round: 1
     }
   }
 
@@ -55,19 +55,59 @@ class QuestionHandler extends Component {
     });
   }
 
-  handleChange = event => {
-    let x = this.state.category_id + 1
-    this.setState({selected_answer: event.target.value,
-                       category_id: x});
+  buttonSelector = event => {
+    this.setState({selected_answer: event.target.value})
+  }
+
+  submitQuestion = () => {
+    this.checkAnswer();
     this.studentChannel.rightAnswer(this.state.category_id, this.state.level, this.state.round);
   }
+
+  checkAnswer = () => {
+    this.setState({
+      category_id: this.nextCategory(this.state.category_id),
+            level: this.nextRound(this.state.level),
+            round: this.nextLevel(this.state.round, this.state.selected_answer, this.state.correct_answer)
+    });
+  }
+
+  nextCategory = (currentCategory) => {
+    console.log(currentCategory)
+    if (currentCategory === 4) {
+      return 1;
+    } else {
+      return currentCategory + 1;
+    }
+  }
+
+  nextRound = (currentRound) => {
+    if (currentRound === 4) {
+      return 0;
+    } else {
+      return currentRound + 1;
+    }
+  }
+
+  nextLevel = (currentLevel, selected_answer, correct_answer) => {
+    console.log(selected_answer, correct_answer)
+    if ((selected_answer === correct_answer && currentLevel === 3) || (selected_answer !== correct_answer && currentLevel === 0 )) {
+      return currentLevel;
+    } else if (selected_answer === correct_answer) {
+      return currentLevel + 1;
+    } else if (selected_answer !== correct_answer) {
+      return currentLevel - 1;
+    }
+  }
+
+
 
   render = () => {
     if (!this.state.qtext) {
       return (
         <div>
          <img src={logo} className="App-logo" alt="logo" />
-         <input type='button' value='Get Question' onClick={this.handleChange}>
+         <input type='button' value='Get Question' onClick={this.submitQuestion}>
          </input>
          <h2> {this.state.selected_answer} </h2>
         </div>
@@ -77,17 +117,37 @@ class QuestionHandler extends Component {
         <div>
           <h1> {this.state.qtext} </h1>
           <br/> <br/>
-          <input type='button' value={this.state.a1} onClick={this.handleChange}>
-          </input>
-          <p/>
-          <input type='button' value={this.state.a2} onClick={this.handleChange}>
-          </input>
-          <p/>
-          <input type='button' value={this.state.a3} onClick={this.handleChange}>
-          </input>
-          <p/>
-          <input type='button' value={this.state.a4} onClick={this.handleChange}>
-          </input>
+          <form>
+            <label>
+              <input type='radio' value={this.state.a1}
+                checked={this.state.selected_answer === this.state.a1}
+                onChange={this.buttonSelector}/>
+              {this.state.a1}
+            </label>
+            <p/>
+            <label>
+              <input type='radio' value={this.state.a2}
+                checked={this.state.selected_answer === this.state.a2}
+                onChange={this.buttonSelector}/>
+              {this.state.a2}
+            </label>
+            <p/>
+            <label>
+              <input type='radio' value={this.state.a3}
+                checked={this.state.selected_answer === this.state.a3}
+                onChange={this.buttonSelector}/>
+              {this.state.a3}
+            </label>
+            <p/>
+            <label>
+              <input type='radio' value={this.state.a4}
+              checked={this.state.selected_answer === this.state.a4}
+              onChange={this.buttonSelector}/>
+              {this.state.a4}
+            </label>
+            <p/>
+            <button onClick={this.submitQuestion}> SUBMIT! </button>
+          </form>
         </div>
       )
     }
