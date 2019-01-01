84648be5ac62a67c9351e4505497c7b1b7151429
Aaron Lai <aaron_lai@hotmail.com>
2018-12-31 22:22:43 -0500

    Student Online list is now showing, it is not dynamic yet (no offline removal) and is based off the studentID from the DB

--- frontend/src/teacher/realTimeTest.jsx
+++ frontend/src/teacher/realTimeTest.jsx
@@ -30,20 +30,21 @@ class RealTimeTest extends Component {
 //  This is for question answers, not used yet
 //  Not fully sure how to build yet either
 //  I'm using the length of the data array for identification
-        if (data.length === 5) {
-          if (this.studentExists(this.state.students, studentId)) {
-            const dataOrganizer = { category: data[1],
-                                       level: data[2],
-                                 correct_ans: data[3],
-                                   wrong_ans: data[4]
-                                  }
-            let studentList = [...this.state.students, dataOrganizer]
-            this.setState({students: studentList})
-          }
+        // if (data.length === 5) {
+          // if (this.studentExists(this.state.students, studentId)) {
+            // const dataOrganizer = { category: data[1],
+            //                            level: data[2],
+            //                      correct_ans: data[3],
+            //                        wrong_ans: data[4]
+            //                       }
+            // let studentList = [...this.state.students, dataOrganizer]
+            // this.setState({students: studentList})
+          // }
 //  For initial Student Online List
 //  the {[]} in dataOrganizer lets me use studentId as a variable
 // as opposed to string
-        } else if (data.length === 2) {
+        // } else
+        if (data.length === 2) {
           if (!this.studentExists(this.state.students, studentId)) {
             const dataOrganizer = { [studentId]: { name: data[1] } };
             let studentList = [...this.state.students, dataOrganizer]
